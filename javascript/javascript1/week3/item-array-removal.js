cbef115c186a1c31a02d4cb9c400b2181eafde74
Ramanzada <zadaray99@gmail.com>
2022-12-16 00:29:42 +0100

    item array removal is finished

---
+++ javascript/javascript1/week3/item-array-removal.js
@@ -0,0 +1,17 @@
+//Item arry removel
+constnames = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+  const nameToRemove = "Ahmad";
+  const names = [Peter, Ahmad, Yana, Kristina, Rasmus, Samuel ,katrine ,Tala];
+
+delete names[1];
+
+console.log(`names values: ${names}`);
