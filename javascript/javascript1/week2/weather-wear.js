9f57515e81e76ff7f6c71ad035883daca99cee18
Ramanzada <zadaray99@gmail.com>
2022-12-15 23:48:03 +0100

    weather-wear is finished

---
+++ javascript/javascript1/week2/weather-wear.js
@@ -0,0 +1,11 @@
+// weather wear
+
+function WeatherDressCode(tempearature) {
+    if (tempearature < 18) {
+      console.log("wear jacket");
+    } else if (tempearature > 18) {
+      console.log("wear shorts and shirts");
+    }
+  }
+  WeatherDressCode(18);
+  WeatherDressCode(10);
