20f29ee52d65418c12e8ceb84aa9a02304e0986a
Ramanzada <zadaray99@gmail.com>
2022-12-15 23:45:39 +0100

    event-application is finished

---
+++ javascript/javascript1/week2/event-application.js
@@ -0,0 +1,18 @@
+// Event Application
+const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  function getEventWeekday(daysLeft) {
+    const today = new Date();
+    const dayIndex = (today.getDay() + daysLeft) % 7;
+    console.log("The event will be held on " + weekDays[dayIndex] + "  ");
+  }
+  
+  getEventWeekday(9); // Logs out "Monday"
+  getEventWeekday(3); // Logs out "Tuesday"
