4cc104d9b47f55ccee3e51b7c0a1af7e98f36382
Ramanzada <zadaray99@gmail.com>
2023-04-01 22:42:59 +0200

    Warmup for Nodejs week 1

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,17 +1,25 @@
-function isNumeric(value) {
-    return !isNaN(parseFloat(value)) && isFinite(value);
+
+function calculateAverage(numbers) {
+  const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  return sum / numbers.length;
+}
+
+
+const args = process.argv.slice(2).map(arg => {
+  const number = parseFloat(arg);
+  if (isNaN(number)) {
+    console.error(`'${arg}' is not a valid number. Please provide only numbers as arguments.`);
+    process.exit(1);
   }
-  
-  const args = process.argv.slice(2);
-  const validNumbers = args.filter(isNumeric).map(Number);
-  
-  if (args.length === 0) {
-    console.log('No arguments provided. Please provide numbers as command line arguments.');
-  } else if (validNumbers.length === 0) {
-    console.log('No valid numbers provided. Please provide numbers as command line arguments.');
-  } else {
-    const sum = validNumbers.reduce((acc, current) => acc + current, 0);
-    const average = sum / validNumbers.length;
-    console.log('Average:', average);
-  }
-  
+  return number;
+});
+
+
+if (args.length === 0) {
+  console.error('No arguments provided. Please provide at least one number as an argument.');
+  process.exit(1);
+}
+
+
+const average = calculateAverage(args);
+console.log(average);
